40e5d4e3a2d38070ff02786f5f634b8a71487eae
Alstro20 <sb74851@gmail.com>
2023-10-20 12:06:34 -0500

    fixed how sudoku list works :3

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,21 +1,32 @@
 from math import fmod
 def get_sudoku(width):
-    sudoku = []
 
     numSquares = width**2
+    sudoku = [None]*numSquares
     i=0
     while i in range(0,numSquares):
         column = int(fmod(i, width))+1 # finds column (x coord) of current square
         row = (i // width)+1 # finds row (y coord) of current square
         #print("DEBUG:\n    column: "+str(column)+"\n    row: "+str(row)) #debug: print squares
         try:
-            print("Enter cell "+str(i)+" ("+str(column)+","+str(row)+")")
+            print("Enter cell "+str(i+1)+" ("+str(column)+","+str(row)+"), or type \"back\"")
             value = input()
             # TODO: add way to go back if an error is made
             #sudoku[i] = [column, row, value]
-            
-            sudoku.insert(i, [column, row, value])
-            i=i+1
+            if value.isnumeric():
+                #sudoku.insert(i, [column, row, value])
+                sudoku[i] = [column, row, value]
+                i=i+1
+            elif value == "":
+                #sudoku.insert(i, [column, row, None])
+                sudoku[i] = [column, row, value]
+                i=i+1
+            elif value == "back" and i != 0:
+                print("going back...")
+                i = i - 1
+            else:
+                print("error, invalid input for cell "+str(i)+" ("+str(column)+","+str(row)+")")
+
         except:
             print("error, invalid input for cell "+str(i)+" ("+str(column)+","+str(row)+")")
             #i = i-1
