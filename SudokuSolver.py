ce22f31054fdd2c30c28f8f11e1ff6a4b1a879c2
Alstro20 <sb74851@gmail.com>
2023-10-20 11:49:17 -0500

    got get_sudoku mostly working

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -3,11 +3,29 @@ def get_sudoku(width):
     sudoku = []
 
     numSquares = width**2
-
-    for i in range(0,numSquares):
+    i=0
+    while i in range(0,numSquares):
         column = int(fmod(i, width))+1 # finds column (x coord) of current square
         row = (i // width)+1 # finds row (y coord) of current square
-        print("DEBUG:\n    column: "+str(column)+"\n    row: "+str(row)) #debug: print squares
+        #print("DEBUG:\n    column: "+str(column)+"\n    row: "+str(row)) #debug: print squares
+        try:
+            print("Enter cell "+str(i)+" ("+str(column)+","+str(row)+")")
+            value = input()
+            # TODO: add way to go back if an error is made
+            #sudoku[i] = [column, row, value]
+            
+            sudoku.insert(i, [column, row, value])
+            i=i+1
+        except:
+            print("error, invalid input for cell "+str(i)+" ("+str(column)+","+str(row)+")")
+            #i = i-1
+    
+    return sudoku
+
+def print_sudoku(sudoku):
+    print("printing current sudoku...")
+    for i in range(len(sudoku)):
+        print("("+str(sudoku[i][0])+","+str(sudoku[i][1])+") = "+str(sudoku[i][2]))
 
 
 
@@ -17,7 +35,8 @@ def main():
     '''main function
     '''
 
-    get_sudoku(9)
+    sud = get_sudoku(3)
+    print_sudoku(sud)
 
 
 if __name__ == '__main__':
